4210a8e48c2f7338a8a0c4ed606d56a921e9f125
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-11-07 02:09:11 +0000

    feat: add user routes at '/api/users'

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import toDoRoutes from './routes/todo.routes'
+import toDoRoutes from "./routes/todo.routes";
+import userRoutes from "./routes/user.routes";
 import { errorHandler } from "./middlewares/error.middleware";
 
 const app: Application = express();
@@ -12,13 +13,14 @@ app.use(morgan('dev'));
 //security headers with helmet
 app.use(helmet());
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true, limit: "50mb"}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: "50mb"}));
 
 
 // specify routes
-app.use('/api/todos', toDoRoutes)
+app.use('/api/todos', toDoRoutes);
+app.use('/api/users', userRoutes);
 app.use(errorHandler);
 
 export default app;
