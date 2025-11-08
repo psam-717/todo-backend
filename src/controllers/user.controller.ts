115f185afda30d63ed5f72aea39a82169e68a81f
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-11-08 23:37:26 +0000

    feat(auth): add login, logout, and getUser controllers with secure cookie handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { signupService } from "../services/user.service";
-
+import { getUserService, loginService, signupService } from "../services/user.service";
+import { ENV } from "../config/env";
 
 export const signupController = async(
     req:Request, 
@@ -21,5 +21,74 @@ export const signupController = async(
     } catch (error) {
         console.log("Error caused by ", (error as Error).message)
         next(error)
+    }
+}
+
+
+export const loginController = async(
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const {email, password} = req.body;
+        const {user, token} = await loginService(email, password);
+
+        const isProduction = ENV.APP.NODE_ENV === "production";
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: isProduction,
+            sameSite: isProduction ? "strict": "lax",
+            maxAge: 5 * 60 * 60 * 1000,
+            path: "/"
+        });
+
+        const {password: _, ...safeUser} = user;
+        res.status(200).json({message: "Login successful", user: safeUser});
+    } catch (error) {
+        console.log("Error caused by ", (error as Error).message);
+        next(error);
+    }
+}
+
+export const logoutController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const isProduction = ENV.APP.NODE_ENV === "production"
+        res.clearCookie(
+            "auth_token", {
+                httpOnly: true,
+                secure: isProduction,
+                sameSite: isProduction ? "strict": "lax",
+                path: "/"
+            }
+        );
+
+        res.status(200).json({message: "Logout successful"});
+    } catch (error) {
+        console.log("Internal server error caused by ", (error as Error).message);
+        next(error);
+    }
+}
+
+export const getUserController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise <void> => {
+    try {
+        const userId = (req as any).user.id;
+
+        const result = await getUserService(userId);
+
+        res.status(200).json({message: "user data retrieved", result});
+        
+    } catch (error) {
+        console.log("Error caused by ", (error as Error).message);
+        next(error);
     }
 }
