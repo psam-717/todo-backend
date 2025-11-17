fe66060e6bd8d2758fbfa0ac23c522abab5357c7
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-11-17 18:07:29 +0000

    second commit

--- dist/routes/health.routes.js
+++ dist/routes/health.routes.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const health_controller_1 = require("../controllers/health.controller");
 const router = (0, express_1.Router)();
-router.get('/', health_controller_1.healthMessage);
+router.get('/health', health_controller_1.healthMessage);
 exports.default = router;
 //# sourceMappingURL=health.routes.js.map
